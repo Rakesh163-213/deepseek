2fdf9a8ddf00f278b45d9980f537d1f3a1fe216d
Rakesh163-213 <naampata35217793@gmail.com>
2025-02-05 23:32:47 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+API_ID =  20967612       # Your Telegram API ID
+API_HASH = "be9356a3644d1e6212e72d93530b434f"  # Your Telegram API Hash
+BOT_TOKEN = "7535985391:AAEfjYY3Z79OvPgQCn3rKZ192jAED9dzeHQ"
+LOG_CHANNEL = -1002429217502  # Your log channel ID
+ADMINS = [7518638302]    # Admin user IDs
